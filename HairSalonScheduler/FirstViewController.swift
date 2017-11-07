7eaf80297a1224c3460c6cf8b7702cb0affb2b6c
Paul Mason <paulmason@kaylies-macbook-pro.local>
2017-11-07 12:33:20 -0700

    Initial Commit

---
+++ HairSalonScheduler/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HairSalonScheduler
+//
+//  Created by Paul Mason on 11/7/17.
+//  Copyright © 2017 Paul Mason. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HairSalonScheduler/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HairSalonScheduler
+//
+//  Created by Paul Mason on 11/7/17.
+//  Copyright © 2017 Paul Mason. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
