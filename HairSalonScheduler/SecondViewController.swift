46d68abc1dcf617a75c79cf0f497f5d8eaca2d82
Gregory Harston <gregory@Gregorys-MacBook-Air.local>
2017-11-07 17:21:13 -0700

    first commit of photo upload, first and last name, gender selector

--- HairSalonScheduler/SecondViewController.swift
+++ HairSalonScheduler/SecondViewController.swift
@@ -8,18 +8,100 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    // image view for photo upload
+    @IBOutlet var imageView: UIImageView!
+    
+    // variables
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    
+    @IBOutlet var genderTextField: UITextField!
+    // gender array
+    let gender = ["Male",
+                  "Female"]
+    // needed variable to save gender input
+    var selectedGender: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createGenderPicker()
+        createToolBar()
     }
 
+    @IBAction func takePhoto(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        image.allowsEditing = false
+        
+        self.present(image, animated: true, completion: nil)
+    }
+    
+    func createGenderPicker() {
+        let genderPicker = UIPickerView()
+        genderPicker.delegate = self
+        
+        genderTextField.inputView = genderPicker
+    }
+    
+    func createToolBar() {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(SecondViewController.dismissKeyboard))
+        
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        genderTextField.inputAccessoryView = toolBar
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    // function for picking the image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            imageView.image = image
+        }
+        else
+        {
+            // error message
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+}
+extension SecondViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return gender.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return gender[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedGender = gender[row]
+        genderTextField.text = selectedGender
+    }
 }
 
