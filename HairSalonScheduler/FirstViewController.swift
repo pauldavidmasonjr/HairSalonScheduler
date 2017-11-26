2f827f5c1e882793f0ac8cde1db3b113d481b71f
Paul Mason <paulmason@kaylies-macbook-pro.local>
2017-11-26 09:27:03 -0700

    still working

--- HairSalonScheduler/FirstViewController.swift
+++ HairSalonScheduler/FirstViewController.swift
@@ -7,10 +7,29 @@
 //
 
 import UIKit
+import Firebase
+
 
 class FirstViewController: UIViewController {
+    
+    //firebase DATABASE variable
+    var refDatabase: DatabaseReference!
+    
+    var pulledData: UInt!
 
     override func viewDidLoad() {
+        
+        //Firebase DATABASE Connection Code
+        FirebaseApp.configure()
+        
+        refDatabase = Database.database(url: "https://hairsalonscheduler.firebaseio.com/").reference(fromURL: "https://hairsalonscheduler.firebaseio.com/").child("Clients")
+        
+        //pulledData = refDatabase.observe(DataEventType.value, with: { (snapshot) in
+          //  _ = snapshot.value as? [String : AnyObject] ?? [:]
+            // ...
+        //})
+        //refHandle = refDatabase.observe(.value, with
+                
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
